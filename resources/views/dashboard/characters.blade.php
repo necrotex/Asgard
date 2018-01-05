856dbc42fd73534b36d4132389d0d5af8503d8d4
Andre Peiffer <necrotex@gmail.com>
2018-01-05 01:31:28 +0100

    wip

--- resources/views/dashboard/characters.blade.php
+++ resources/views/dashboard/characters.blade.php
@@ -4,26 +4,50 @@
 @section('CONTENT_TITLE', 'Characters')
 
 @section('content')
-    <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
+    <div class="row">
 
-            <div class="panel-body">
-                <a href="{{route('sso.login')}}">Add Account</a>
+        <div class="col-md-8 col-md-offset-2">
 
-                <ul class="list-group">
+            @foreach($characters as $character)
+                <div class="card character-card">
+                    <div class="card-body">
 
-                    @foreach($characters as $character)
-                        <li class="list-group-item">
-                            <img class="media-object avatar" src="https://image.eveonline.com/Character/{{$character->id}}_64.jpg"/>
+                        <div class="row">
+                           <div class="col-md-1">
+                               <img class="media-object avatar"
+                                    src="https://image.eveonline.com/Character/{{$character->id}}_64.jpg"/>
+                           </div>
 
-                            {{$character->name}}
+                            <div class="col-md-10">
+                                {{$character->name}}
+                                <br />
+                                {{$character->owner_hash}}
 
-                            <div class="clearfix"></div>
-                        </li>
-                    @endforeach
-                </ul>
+                            </div>
 
-            </div>
+                            <div class="col-md-1 status-col">
+                                <div class="status status-success pull-right">
+                                    <i class="fa fa-check" aria-hidden="true"></i>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
+            @endforeach
+
+
+        </div>
+
+        <div class="col-md-4">
+
+            <a href="{{route('sso.login')}}" class="btn btn-primary btn-lg btn-block" role="button" aria-pressed="true">Add <Charcater></Charcater></a>
+
         </div>
     </div>
+
 @endsection
+
