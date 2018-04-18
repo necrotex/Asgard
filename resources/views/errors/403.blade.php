45fa5e8908c7507c283fae2c287bf567c524c635
Andre Peiffer <necrotex@gmail.com>
2018-04-18 13:17:22 +0200

    wip

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+    <div class="logo-container">
+        <img id="profile-img" class="profile-img-card" src="/img/logo.png"/>
+
+        <h1 class="text-center hero">403</h1>
+        <h1 class="text-center hero">Not Authorized</h1>
+    </div>
+
+
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+    <div class="logo-container">
+        <img id="profile-img" class="profile-img-card" src="/img/logo.png"/>
+
+        <h1 class="text-center hero">404</h1>
+        <h1 class="text-center hero">Page not Found</h1>
+    </div>
+
+
+@endsection
