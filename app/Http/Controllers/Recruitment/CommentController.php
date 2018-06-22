2be5349d99267f9d0a1caa127f21ae9c9ae4e874
Andre Peiffer <necrotex@gmail.com>
2018-06-22 12:50:06 +0200

    added application comments controller

---
+++ app/Http/Controllers/Recruitment/CommentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Asgard\Http\Controllers\Recruitment;
+
+use Asgard\Models\ApplicationComment;
+use Illuminate\Http\Request;
+use Asgard\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Asgard\Models\ApplicationComment  $applicationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ApplicationComment $applicationComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Asgard\Models\ApplicationComment  $applicationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ApplicationComment $applicationComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Asgard\Models\ApplicationComment  $applicationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ApplicationComment $applicationComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Asgard\Models\ApplicationComment  $applicationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ApplicationComment $applicationComment)
+    {
+        //
+    }
+}
--- resources/views/dashboard/partials/profile/characters.blade.php
+++ resources/views/dashboard/partials/profile/characters.blade.php
@@ -4,19 +4,22 @@
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="col-12 col-lg-8 col-md-6">
-                    <h3 class="mb-0 text-truncated">
-                        <a href="{{route('characters.show', $character->id)}}">
-                            {{$character->name}}
-                        </a>
-                    </h3>
-                    <p class="lead">{{optional($character->corporation)->name}}</p>
 
-                </div>
-                <div class="col-12 col-lg-4 col-md-6 text-center">
-                    <img src="https://image.eveonline.com/Character/{{$character->id}}_128.jpg" width="100" alt="{{$character->name}}"
+                <div class="col-2 text-center">
+                    <img src="https://image.eveonline.com/Character/{{$character->id}}_128.jpg" width="50"
+                         alt="{{$character->name}}"
                          class="mx-auto rounded-circle img-fluid">
                 </div>
+                <div class="col-10">
+
+                    <h5 class="card-title"><a href="{{route('characters.show', $character->id)}}">
+                            {{$character->name}}
+                        </a></h5>
+
+                    <h6 class="card-subtitle mb-2 text-muted">{{optional($character->corporation)->name}}</h6>
+
+                </div>
+
             </div>
             <!--/row-->
         </div>
