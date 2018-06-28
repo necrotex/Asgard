fda999df836f15edad93fc3d547d499c85e59a9e
Andre Peiffer <necrotex@gmail.com>
2018-06-28 09:05:21 +0200

    a user doesn't need permissions to link their discord account. fixes #7

--- app/Http/Controllers/Service/DiscordController.php
+++ app/Http/Controllers/Service/DiscordController.php
@@ -14,11 +14,6 @@ use Laravel\Socialite\Facades\Socialite;
 
 class DiscordController extends Controller
 {
-    public function __construct()
-    {
-        $this->authorizeResource(DiscordUser::class);
-    }
-
     /**
      * Display a listing of the resource.
      *
