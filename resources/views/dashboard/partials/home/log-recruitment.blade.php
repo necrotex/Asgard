9947f16264860afcdb38202564cb3a38743aea98
Andre Peiffer <necrotex@gmail.com>
2018-07-05 10:09:17 +0200

    Fixed wrong system messages link. fixes #24

--- resources/views/dashboard/partials/home/log-recruitment.blade.php
+++ resources/views/dashboard/partials/home/log-recruitment.blade.php
@@ -1,7 +1,6 @@
 <li class="list-group-item list-group-item list-group-item-primary">
     <b>Recruitment</b>: <a
-            href="{{route('applications.show', $message->causer)}}">{{$message->description}} {{$message->subject->name}}
-        for {{$message->causer->mainCharacter->name}}</a>
+            href="{{route('applications.show', $message->subject)}}">{{$message->description}}</a>
     <br>
     <small class="text-muted">{{$message->created_at}}</small>
 </li>
