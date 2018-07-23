9b28d88b777dd12cf4ed1ee309e8a98c4214da0f
Andre Peiffer <necrotex@gmail.com>
2018-07-23 13:48:06 +0200

    Added backup packaged and deploy script

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -59,6 +59,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('asgard:discord:fetch-roles')->daily();
         $schedule->command('activitylog:clean')->daily();
 
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
