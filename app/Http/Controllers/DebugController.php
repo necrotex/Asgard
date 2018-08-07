e547e2ce4c9868e2658fcfd02957764a5bc96e72
Andre Peiffer <necrotex@gmail.com>
2018-08-07 10:58:29 +0200

    refactored discord integration

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -2,6 +2,7 @@
 
 namespace Asgard\Http\Controllers;
 
+use Asgard\Jobs\Discord\Rename;
 use Asgard\Jobs\Discord\UpdateUserRolesJob;
 use Asgard\Models\Character;
 use Asgard\Models\User;
@@ -12,6 +13,6 @@ class DebugController extends Controller
     public function index()
     {
         $char = User::find(1);
-        dispatch_now(new UpdateUserRolesJob($char));
+        dispatch_now(new Rename($char));
     }
 }
