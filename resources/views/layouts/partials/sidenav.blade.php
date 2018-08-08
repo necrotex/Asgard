6e9bddf1f784231953230d95e2bde6f412e04e0f
Andre Peiffer <necrotex@gmail.com>
2018-08-08 14:37:17 +0200

    changed sidenav access policy

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -25,7 +25,7 @@
                 </li>
             @endcan
 
-            @can('create-feedback')
+            @can('create', \Asgard\Models\Feedback::class)
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('feedback.create')}}">
                         <i class="fas fa-comment-alt" aria-hidden="true"></i> Feedback
