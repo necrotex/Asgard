38f5c93bc84bdf19199146cac63686a19cd9e0e4
Andre Peiffer <necrotex@gmail.com>
2018-08-13 10:51:15 +0200

    added expired token notice to character infos

--- resources/views/dashboard/character.blade.php
+++ resources/views/dashboard/character.blade.php
@@ -18,42 +18,60 @@
     </nav>
 
     <div class="row">
-
         <div class="col-3">
-            <div class="card">
-                <div class="profile-image">
-                    <img src="https://image.eveonline.com/Character/{{$character->id}}_512.jpg" class="card-img-top">
-                    <div class="profile-overlay text-center">
-                        @if($character->status->online)
-                            <span class="profile-overlay-text text-success">ONLINE</span>
-                        @else
-                            <span class="profile-overlay-text text-danger">OFFLINE</span>
-                        @endif
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="profile-image">
+                            <img src="https://image.eveonline.com/Character/{{$character->id}}_512.jpg"
+                                 class="card-img-top">
+                            <div class="profile-overlay text-center">
+                                @if($character->status->online)
+                                    <span class="profile-overlay-text text-success">ONLINE</span>
+                                @else
+                                    <span class="profile-overlay-text text-danger">OFFLINE</span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="card-body text-center">
+
+                            <h4 class="card-title">{{$character->name}}</h4>
+                            <p class="card-text">{{$character->corporation->name}}</p>
+
+                            <p class="card-text text-muted">
+                                {{$character->location->solarSystem->solarSystemName}}
+                                <br>
+                                {{$character->location->shipType->typeName}} ({{$character->location->ship_name}})
+                            </p>
+                        </div>
+                        <div class="card-footer text-center">
+                            <a href="https://zkillboard.com/character/{{$character->id}}/" title="zKillboard"
+                            >
+                                <i class="asgard-icon asgard-icon-zkill"></i>
+                            </a>
+                            <a href="https://evewho.com/pilot/{{str_replace(' ', '+', $character->name)}}"
+                               title="EveWho">
+                                <i class="asgard-icon asgard-icon-evewho"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
 
 
-                <div class="card-body text-center">
-
-                    <h4 class="card-title">{{$character->name}}</h4>
-                    <p class="card-text">{{$character->corporation->name}}</p>
-
-                    <p class="card-text text-muted">
-                        {{$character->location->solarSystem->solarSystemName}}
-                        <br>
-                        {{$character->location->shipType->typeName}} ({{$character->location->ship_name}})
-                    </p>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="https://zkillboard.com/character/{{$character->id}}/" title="zKillboard"
-                    >
-                        <i class="asgard-icon asgard-icon-zkill"></i>
-                    </a>
-                    <a href="https://evewho.com/pilot/{{str_replace(' ', '+', $character->name)}}" title="EveWho">
-                        <i class="asgard-icon asgard-icon-evewho"></i>
-                    </a>
-                </div>
             </div>
+
+            @if(!$character->active)
+                <div class="row">
+                    <div class="col-12">
+                        <div class="alert-danger alert mt-2">
+                            The Character token is expired, please re-add the character to the system!
+                        </div>
+                    </div>
+                </div>
+            @endif
+
         </div>
 
         <div class="col-9">
